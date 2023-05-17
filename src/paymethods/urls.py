a433bb8910b8e85142d3b6b2b0c25a2e83195cf9
ExpertNSK <expertnsk89@gmail.com>
2023-05-17 17:54:39 +0700

    app paymethods is Done

--- src/paymethods/urls.py
+++ src/paymethods/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ListPayMethod, create_payment_type, create_paymethod
+from .views import ListPayMethod, create_payment_type, create_paymethod, delete_payment_type, edit_payment_type
 
 
 app_name = 'paymethods'
@@ -9,5 +9,7 @@ urlpatterns = [
     path('list/', ListPayMethod.as_view(), name='list'),
     path('create/<int:pk>', create_paymethod, name='create'),
     path('pmtype_add/', create_payment_type, name='pmtype_add'),
+    path('pmtype_delete', delete_payment_type, name='pmtype_delete'),
+    path('pmtype_edit', edit_payment_type, name='pmtype_edit'),
 ]
 
--- src/paymethods/views.py
+++ src/paymethods/views.py
@@ -5,12 +5,17 @@ from django.views.decorators.csrf import csrf_exempt
 
 from loaders.models import Loader
 from .forms import PayMethodForm, PaymentTypeForm
-from .models import PayMethod, PaymentType
+from .models import PaymentType
 
 
 class ListPayMethod(ListView):
-    queryset = PayMethod.objects.all()
+    queryset = PaymentType.objects.all()
     template_name = 'paymethods/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PaymentTypeForm()
+        return context
 
 
 def create_paymethod(request, pk):
@@ -35,11 +40,38 @@ def create_paymethod(request, pk):
 @csrf_exempt
 def create_payment_type(request):
     if request.method == 'POST':
+        print(request.POST)
         form = PaymentTypeForm(request.POST)
         if form.is_valid():
-            form.save()
+            payment_type = request.POST['payment_type']
+            pid = request.POST.get('id')
+            if pid == '':
+                pmtype = PaymentType(payment_type=payment_type)
+            else:
+                pmtype = PaymentType(id=pid, payment_type=payment_type)
+            pmtype.save()
             payment_types = PaymentType.objects.values()
             payment_type_data = list(payment_types)
             return JsonResponse({'status': 'OK', 'payment_types': payment_type_data})
         return JsonResponse({'status': 'Unable to save'})
+    return JsonResponse({'status': f'405. Method {request.method} is not allowed.'})
+
+@csrf_exempt
+def delete_payment_type(request):
+    if request.method == 'POST':
+        pid = request.POST['pid']
+        if PaymentType.objects.filter(pk=pid).exists():
+            PaymentType.objects.filter(pk=pid).delete()
+            return JsonResponse({'status': 'OK'})
+        return JsonResponse({'status': f'Object with pid = {pid} is not found'})
+    return JsonResponse({'status': f'405. Method {request.method} is not allowed.'})
+
+def edit_payment_type(request):
+    if request.method == 'GET':
+        pid = request.GET['pid']
+        if PaymentType.objects.filter(pk=pid).exists():
+            payment_type = PaymentType.objects.filter(pk=pid).get()
+            payment_type_data = {'id': payment_type.id, 'payment_type': payment_type.payment_type}
+            return JsonResponse({'status': 'OK', 'payment_type': payment_type_data})
+        return JsonResponse({'status': f'Object with pid = {pid} is not found'})
     return JsonResponse({'status': f'405. Method {request.method} is not allowed.'})
