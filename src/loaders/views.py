72c9122bab3b14b7fa3a0236e00bc9120102742d
ExpertNSK <expertnsk89@gmail.com>
2023-05-17 22:59:25 +0700

    apps specs, statuses, pay methods Done

--- src/loaders/views.py
+++ src/loaders/views.py
@@ -4,6 +4,8 @@ from django.views.generic import CreateView, ListView, UpdateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
+from statuses.forms import StatusForm
+from specializations.forms import SpecializationForm
 from passports.forms import PassportForm
 from loaders.models import Loader
 from loaders.forms import CreateLoaderForm
@@ -24,6 +26,12 @@ class CreateLoaderView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     def get_success_url(self):
         create_call_result(self.object.id)
         return reverse_lazy('loaders:detail', args=(self.object.id,))
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['specs_form'] = SpecializationForm
+        context['status_form'] = StatusForm
+        return context
 
 
 class DetailLoaderView(LoginRequiredMixin, DetailView):
